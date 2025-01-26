de0790d02cdfd2223629960c1cd40f20d9a6f353
Nadav Benyakar <nadavby23351@gmail.com>
2025-01-26 15:22:46 +0200

    new

--- my-app/files/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS events (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    start_time DATETIME NOT NULL,
-    end_time DATETIME NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
